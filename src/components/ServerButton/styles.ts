3c37f6a2b890523249fcc45e35dca593cc75db84
Filipe Dias <filipediascontato@gmail.com>
2022-04-04 17:13:47 -0300

    Implements ServerButton styles

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -13,7 +13,7 @@ export const Button = styled.button<Props>`
   border-radius: 50%;
 
   margin-bottom: 8px;
-  background-color: ${(props) =>
+  background-color: ${props =>
     props.isHome ? "var(--rocketseat)" : "var(--primary)"};
 
   cursor: pointer;
@@ -25,9 +25,42 @@ export const Button = styled.button<Props>`
   }
 
   &::before {
+    width: 9px;
+    height: 9px;
+
+    position: absolute;
+    left: -17px;
+    top: calc(50% - 4.5px);
+
+    background-color: var(--white);
+    border-radius: 50%;
+
+    content: "";
+    display: ${props => (props.hasNotifications ? "inline" : "none")};
   }
 
   &::after {
+    background-color: var(--notification);
+    width: auto;
+    height: 16px;
+
+    padding: 0 4px;
+
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    border-radius: 12px;
+    border: 4px solid var(--quaternary);
+
+    text-align: right;
+    font-size: 13px;
+    font-weight: bold;
+    color: var(--white);
+
+    content: "${props => props.mentions && props.mentions}";
+    display: ${props =>
+    props.mentions && props.mentions > 0 ? "inline" : "none"};
   }
 
   transition: border-radius 0.2s, background-color 0.2s;
@@ -35,7 +68,7 @@ export const Button = styled.button<Props>`
   &.active,
   &:hover {
     border-radius: 16px;
-    background-color: ${(props) =>
+    background-color: ${props =>
     props.isHome ? "var(--rocketseat)" : "var(--discord)"};
   }
 `;
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <h1>ServerName</h1>
+    </Container>
+  );
+};
+
+export default ServerName;
